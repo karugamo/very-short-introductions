b4abcf7efa9d68103db9499ebc32cc49212f9c32
Finn Pauls <mbp@pauls.dev>
2022-05-17 12:26:24 +0200

    Show books with ratings

--- fetch-books.ts
+++ fetch-books.ts
@@ -1,42 +1,47 @@
-import got from 'got'
-import { JSDOM } from 'jsdom'
-import {writeFileSync} from 'fs'
+import { writeFileSync } from "fs";
+import got from "got";
+import { JSDOM } from "jsdom";
 
-const {body} = await got('https://www.goodreads.com/list/show/43502.The_Oxford_Very_Short_Introductions_Series')
+const { body } = await got(
+  "https://www.goodreads.com/list/show/43502.The_Oxford_Very_Short_Introductions_Series"
+);
 
-const document = new JSDOM(body).window.document
+const document = new JSDOM(body).window.document;
 
+const bookElements = Array.from(
+  document.querySelectorAll('[itemtype="http://schema.org/Book"]')
+);
 
-const bookElements = Array.from(document.querySelectorAll('[itemtype="http://schema.org/Book"]'))
-
-const books = []
+const books = [];
 
 for (const bookElement of bookElements) {
-  const book = await parseBookElement(bookElement)
-  books.push(book)
+  const book = await parseBookElement(bookElement);
+  books.push(book);
 }
 
-writeFileSync('./data/books.json', JSON.stringify(books, null, 2))
-console.log(`✅ Wrote ${books.length} books to data/books.json`)
+writeFileSync("./data/books.json", JSON.stringify(books, null, 2));
+console.log(`✅ Wrote ${books.length} books to data/books.json`);
 
 async function parseBookElement(bookElement: Element) {
-  const title = bookElement.querySelector('[itemprop="name"]').textContent
+  const title = bookElement
+    .querySelector('[itemprop="name"]')
+    .textContent.replace(": A Very Short Introduction", "");
 
-  const image = bookElement.querySelector('a img').getAttribute('src')
+  const image = bookElement.querySelector("a img").getAttribute("src");
 
-  const url = bookElement.querySelector('a').getAttribute('href')
+  const url = bookElement.querySelector("a").getAttribute("href");
 
-  const {rating, numberOfRatings} = parseRatingText(bookElement.querySelector('.minirating').textContent)
+  const { rating, numberOfRatings } = parseRatingText(
+    bookElement.querySelector(".minirating").textContent
+  );
 
-  return {rating, numberOfRatings, url, title, image}
-
+  return { rating, numberOfRatings, url, title, image };
 }
 
-// 3.83 avg rating — 2,193 ratings
 function parseRatingText(text: string) {
-  const [rating, count] = text.replace(',', '').split(' — ')
-  return {
-    rating: parseFloat(rating),
-    numberOfRatings: parseInt(count)
-  }
+  const [rating, numberOfRatings] = text
+    .replace(",", "")
+    .match(/(\d\.\d\d) avg rating — (\d+) ratings/)
+    .map(parseFloat);
+  return { rating, numberOfRatings };
 }
