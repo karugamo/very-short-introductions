8d4a2301ad048bd1406dc6a6a4975148dcb187bc
Finn Pauls <mbp@pauls.dev>
2022-05-17 12:34:28 +0200

    Fetch all books

--- fetch-books.ts
+++ fetch-books.ts
@@ -2,27 +2,20 @@ import { writeFileSync } from "fs";
 import got from "got";
 import { JSDOM } from "jsdom";
 
-const { body } = await got(
-  "https://www.goodreads.com/list/show/43502.The_Oxford_Very_Short_Introductions_Series"
-);
-
-const document = new JSDOM(body).window.document;
-
-const bookElements = Array.from(
-  document.querySelectorAll('[itemtype="http://schema.org/Book"]')
-);
-
-const books = [];
-
-for (const bookElement of bookElements) {
-  const book = await parseBookElement(bookElement);
-  books.push(book);
+let page = 1
+const books = []
+while (true) {
+  console.log(`Fetching page ${page}`)
+  const newBooks = await getBooks(page)
+  page++
+  if (newBooks.length === 0) break;
+  books.push(...newBooks)
 }
 
 writeFileSync("./data/books.json", JSON.stringify(books, null, 2));
 console.log(`✅ Wrote ${books.length} books to data/books.json`);
 
-async function parseBookElement(bookElement: Element) {
+async function parseBookElement(bookElement: Element): Promise<Book> {
   const title = bookElement
     .querySelector('[itemprop="name"]')
     .textContent.replace(": A Very Short Introduction", "");
@@ -44,4 +37,32 @@ function parseRatingText(text: string) {
     .match(/(\d\.\d\d) avg rating — (\d+) ratings/)
     .map(parseFloat);
   return { rating, numberOfRatings };
+}
+
+interface Book {
+  rating: number;
+  numberOfRatings: number;
+  url: string;
+  title: string;
+  image: string;
+}
+
+async function getBooks(page: number): Promise<Book[]> {
+  const { body } = await got(
+    "https://www.goodreads.com/list/show/43502.The_Oxford_Very_Short_Introductions_Series?page=" + page
+  );
+  
+  const document = new JSDOM(body).window.document;
+  
+  const bookElements = Array.from(
+    document.querySelectorAll('[itemtype="http://schema.org/Book"]')
+  );
+
+  const books = [];
+  for (const bookElement of bookElements) {
+    const book = await parseBookElement(bookElement);
+    books.push(book);
+  }
+
+  return books
 }
