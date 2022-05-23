63275583e74c14ea4e30e7d3a0fb7d2cec7d03de
Finn Pauls <mbp@pauls.dev>
2022-05-23 12:58:07 +0200

    Fix vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import { defineConfig } from "vite";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svelte()],
-  base: "/very-short-introductions",
+  base: "/very-short-introductions/",
 });
