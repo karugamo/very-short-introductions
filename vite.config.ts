0a5e4548dcd9aaba038312b47be7745c40feed1f
Finn Pauls <mbp@pauls.dev>
2022-05-23 13:01:17 +0200

    Change order of vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte()],
   base: "/very-short-introductions/",
+  plugins: [svelte()],
 });
